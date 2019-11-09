eef5bdb8cb22dda080a98962290740384e25f652
StanleyWkMan <stanley.wk.man@outlook.com>
2019-11-09 13:08:41 +0800

    Simple checking in SlotMachine

--- src/main/java/core/components/SlotMachine.java
+++ src/main/java/core/components/SlotMachine.java
@@ -3,14 +3,33 @@ package core.components;
 import java.util.ArrayList;
 
 public class SlotMachine {
-    private ArrayList<AbstractSlot> slots;
+//    private ArrayList<AbstractSlot> slots;
+
+    private AbstractSlot[] slots;
+
+    private static SlotMachine slotMachineInstance = new SlotMachine();
+
+    private SlotMachine() {
+        this.slots = new AbstractSlot[3];
+    }
+
+    public static SlotMachine getInstance() {
+        return slotMachineInstance;
+    }
 
     public int spin(int tokens){
 
+        //TODO: random slots, then set left, center, right slots
         return 0;
     }
 
-    private boolean isThreeofAKind() {
+    public boolean isSamePattern() {
+        return this.slots[0].getName().equals(this.slots[1].getName())
+                && this.slots[0].getName().equals(this.slots[2].getName());
+    }
+
+    public boolean isSameColour() {
+        //TODO: add colour enum or interface for each slotItem
         return false;
     }
 
--- src/main/test/core/components/SlotMachineTest.java
+++ src/main/test/core/components/SlotMachineTest.java
@@ -1,4 +1,7 @@
 package core.components;
 
 public class SlotMachineTest {
+
+
+
 }
