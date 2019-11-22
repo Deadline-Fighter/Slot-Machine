5b6f4dfe0dd96d946c842cb7e2134e05b96edfc9
Eddy <cschow22-c@my.cityu.edu.hk>
2019-11-22 15:57:26 +0800

    Modified PlayerController.java

--- src/main/java/core/controller/PlayerController.java
+++ src/main/java/core/controller/PlayerController.java
@@ -4,38 +4,32 @@ import core.components.Player;
 
 public class PlayerController {
     private static final int INITIAL_TOKENS = 100;
-    private Player player;
 
     public PlayerController(String name){
     }
 
-    public Player createPlayer(String name){
-        player = new Player(name, INITIAL_TOKENS);
-        return player;
-    }
-
-    public int addMoney(int money) {
-        int balance = this.player.getTokens();
+    public int addMoney(Player player, int money) {
+        int balance = player.getTokens();
         balance += money;
-        this.player.setTokens(balance);
+        player.setTokens(balance);
 
         return balance;
     }
 
-    public int loseMoney(int money) {
-        int balance = this.player.getTokens();
+    public int loseMoney(Player player, int money) {
+        int balance = player.getTokens();
         balance -= money;
-        this.player.setTokens(balance);
+        player.setTokens(balance);
 
         return balance;
     }
 
-    public boolean isBankrupt(){
-        return this.player.getTokens() <= 0;
+    public boolean isBankrupt(Player player){
+        return player.getTokens() <= 0;
     }
 
-    public int getMoney(){
-        return this.player.getTokens();
+    public int getMoney(Player player){
+        return player.getTokens();
     }
 
 }
